fbdcef350b4cc022faea6010d9b880288fc7b0a5
DENÍLSON MARCIO DOS SANTOS <48038716+DenilsonMarcio@users.noreply.github.com>
2020-10-16 04:28:55 -0300

    new settings

--- moppahDjango/settings.py
+++ moppahDjango/settings.py
@@ -155,7 +155,7 @@ DATABASES = {'default': dj_database_url.config()}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moppahtechplataform.herokuapp.com']
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
